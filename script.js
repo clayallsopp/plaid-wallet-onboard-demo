fc2dd5af04cedb68754d7be64db47f4f63dc6145
Clay Allsopp <callsopp@plaid.com>
2022-10-24 15:36:44 -0700

    working ete

---
+++ script.js
@@ -0,0 +1,70 @@
+var ethersScript = document.createElement("script");
+ethersScript.setAttribute(
+  "src",
+  "https://cdnjs.cloudflare.com/ajax/libs/ethers/5.7.2/ethers.umd.min.js"
+);
+document.head.appendChild(ethersScript);
+
+var mintButton = document.getElementById("mint-btn");
+var mintProvider;
+
+var onConnect = async function (provider) {
+  mintProvider = provider;
+  const accounts = await provider.request({ method: "eth_accounts" });
+
+  mintButton.classList.remove("hidden");
+  connectButton.classList.add("hidden");
+
+  mintButton.innerHTML = `Mint to ${formatAddress(accounts[0])}`;
+};
+
+// via https://github.com/gpxl-dev/truncate-eth-address/blob/main/src/index.ts
+var formatAddress = function (address) {
+  var truncateRegex = /^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/;
+  var match = address.match(truncateRegex);
+  if (!match) return address;
+  return `${match[1]}…${match[2]}`;
+};
+
+var onConnect = async function (provider) {
+  mintProvider = provider;
+  const accounts = await provider.request({ method: "eth_accounts" });
+
+  mintButton.classList.remove("hidden");
+  connectButton.classList.add("hidden");
+
+  mintButton.innerHTML = `Mint to ${formatAddress(accounts[0])}`;
+};
+
+window.mint = async function () {
+  connectButton.setAttribute("disabled", true);
+  var contractAddress = "0xf61f31577e2144adb020fddad12e2fbf61f2ebb2";
+  var contractAbi = JSON.stringify([
+    {
+      inputs: [{ internalType: "uint256", name: "quantity", type: "uint256" }],
+      name: "purchase",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "payable",
+      type: "function",
+    },
+  ]);
+  var ethersProvider = new window.ethers.providers.Web3Provider(mintProvider);
+  var ethersSigner = ethersProvider.getSigner();
+  var contractInstance = new window.ethers.Contract(
+    contractAddress,
+    contractAbi,
+    ethersSigner
+  );
+  try {
+    mintButton.innerHTML = "Confirming...";
+    const txHash = await contractInstance.purchase(1);
+    mintButton.innerHTML = "Minting...";
+    console.log({ txHash });
+    const tx = await txHash.wait();
+    console.log({ tx });
+    mintButton.innerHTML = "Minted!";
+  } catch (e) {
+    console.error(e);
+    mintButton.innerHTML = "Mint error";
+  }
+};
